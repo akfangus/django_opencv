3b9662426f0c65bc6c7f336706baf2132ef628ce
akfangus <akfanakfan@naver.com>
2022-09-16 02:54:43 +0900

    opencv

---
+++ mainapp/views.py
@@ -0,0 +1,40 @@
+import threading
+
+import cv2
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def mainapp(request):
+    return render(request, 'mainapp/main.html')
+
+# 웹캠 클래스 생성
+class VideoCamera(object):
+    # 초기화 함수
+    def __init__(self):
+        # 클래스의 video인자를 만들고 opencv의 VideoCapture값을 저장
+        self.video = cv2.VideoCapture(0)
+
+        (self.grabbed, self.frame) = self.video.read()
+
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        # image에 초기설정한 frame을 가져와서
+        image = self.frame
+        # jpg형태로 인코딩을 해서 저장한다고함.
+        _, jpeg = cv2.imencode('.jpg', image)
+        # 그 인코딩된 jpg를 바이트 형식으로 반환해야한다고함.
+        # 이래야 프레임별로 라이브 영상을 뽑을수 있다고함.
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            # 루프를 돌려서 지속적으로 frame 값을 업데이트 한다고함.
+            (self.grabbed, self.frame) = self.video.read()
+
